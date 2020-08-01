aa8a1777835fc80b025a99949fb224fb05259aa7
jakebolewski <jakebolewski@gmail.com>
2020-07-31 20:22:21 -0700

    add ability to page api queries and cancel builds

--- bin/slurmjob.sh
+++ bin/slurmjob.sh
@@ -4,7 +4,9 @@
 #SBATCH --reservation=clima
 
 BUILDKITE_PATH=/groups/esm/buildkite
+
 ${BUILDKITE_PATH}/bin/buildkite-agent start \
+  --name "central-$1-%n" \
   --config "${BUILDKITE_PATH}/buildkite-agent.cfg" \
-  --acquire-job "$1" \
+  --acquire-job "$2" \
   --tags "jobid=${SLURM_JOB_ID},ntasks=${SLURM_NTASKS}"
