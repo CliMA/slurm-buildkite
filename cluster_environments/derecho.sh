6b9d8c8af46601239f8f8a940747b2ab4a1c9c70
nefrathenrici <nat.henrici@gmail.com>
2026-04-08 12:44:37 -0600

    Update ncarenv for derecho

--- cluster_environments/derecho.sh
+++ cluster_environments/derecho.sh
@@ -8,9 +8,7 @@ export NCAR_DEFAULT_INFOPATH="/usr/local/share/info:/usr/share/info"
 export NCAR_DEFAULT_MANPATH="/usr/local/share/man:/usr/share/man"
 export NCAR_DEFAULT_PATH="/usr/local/bin:/usr/bin:/sbin:/bin"
 export MODULEPATH="/glade/campaign/univ/ucit0011/ClimaModules-Derecho:/glade/u/apps/derecho/modules/environment"
-module load ncarenv/24.12
-
-export PATH="/glade/campaign/univ/ucit0011/software/MPIwrapper/2024_05_27/bin/:$PATH"
+module load ncarenv
 
 # PBS sets TMPDIR to /var/tmp/pbs.${PBS_JOBID}.${HOSTNAME}
 # Ensure this directory exists and persists (Buildkite needs it for hook wrappers)
